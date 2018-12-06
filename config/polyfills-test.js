cb204bd14cf5c8a84d6f59a652f57fbd6ed01a4a
chenglin.xiao <chenglin.xiao@chaitin.com>
2018-12-06 18:27:52 +0800

    feat: fix eslint

--- config/polyfills-test.js
+++ config/polyfills-test.js
@@ -6,7 +6,5 @@ const Adapter = require('enzyme-adapter-react-16')
 configure({ adapter: new Adapter() })
 
 process.chiffon = {
-  head: '2ac4e5d7f6d296d1c8b7fa89ebf0ff5474a1acbf',
-  time: 1527067667089,
-  type: process.env.TYPE || 'C',
+  time: Date().now,
 }
--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,17 @@
 module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  parser: 'typescript',
+  printWidth: 100,
+  proseWrap: 'preserve',
+  requirePragma: false,
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
 }
