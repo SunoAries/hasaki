5b1cac03c92e50409aaafc995e3f7fff326f954d
chenglin.xiao <chenglin.xiao@chaitin.com>
2018-12-07 15:15:42 +0800

    断剑重铸之日，骑士归来之时

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Bar from './Bar'
 
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
@@ -13,6 +12,7 @@ const App = () => (
         Learn React
       </a>
     </header>
+    <Bar />
   </div>
 )
 
