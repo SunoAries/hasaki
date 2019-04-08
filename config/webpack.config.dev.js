1452b022e68b2ed1a4a6289d96112715093d6901
chenglin.xiao <chenglin.xiao@chaitin.com>
2019-04-08 18:09:17 +0800

    chore: remove ts-loader

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -206,24 +206,10 @@ module.exports = {
           // Process application JS with Babel.
           // The preset includes JSX, Flow, and some ESnext features.
           {
-            test: /\.(js|mjs|jsx)$/,
+            test: /\.(ts|tsx)$/,
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              customize: require.resolve('babel-preset-react-app/webpack-overrides'),
-
-              plugins: [
-                [
-                  require.resolve('babel-plugin-named-asset-import'),
-                  {
-                    loaderMap: {
-                      svg: {
-                        ReactComponent: '@svgr/webpack?-prettier,-svgo![path]',
-                      },
-                    },
-                  },
-                ],
-              ],
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
@@ -231,44 +217,6 @@ module.exports = {
               // Don't waste time on Gzipping the cache
               cacheCompression: false,
             },
-          },
-          // Process any JS outside of the app with Babel.
-          // Unlike the application JS, we only compile the standard ES features.
-          {
-            test: /\.(js|mjs)$/,
-            exclude: /@babel(?:\/|\\{1,2})runtime/,
-            loader: require.resolve('babel-loader'),
-            options: {
-              babelrc: false,
-              configFile: false,
-              compact: false,
-              presets: [
-                [require.resolve('babel-preset-react-app/dependencies'), { helpers: true }],
-              ],
-              cacheDirectory: true,
-              // Don't waste time on Gzipping the cache
-              cacheCompression: false,
-
-              // If an error happens in a package, it's possible to be
-              // because it was compiled. Thus, we don't want the browser
-              // debugger to show the original code. Instead, the code
-              // being evaluated would be much more helpful.
-              sourceMaps: false,
-            },
-          },
-          // TypeScript
-          {
-            test: /\.(ts|tsx)$/,
-            include: paths.appSrc,
-            use: [
-              {
-                loader: require.resolve('ts-loader'),
-                options: {
-                  transpileOnly: true,
-                  experimentalWatchApi: true,
-                },
-              },
-            ],
           },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -279,43 +279,17 @@ module.exports = {
           // Process application JS with Babel.
           // The preset includes JSX, Flow, TypeScript and some ESnext features.
           {
-            test: /\.(js|mjs|jsx)$/,
+            test: /\.(ts|tsx)$/,
             include: paths.appSrc,
 
             loader: require.resolve('babel-loader'),
             options: {
-              customize: require.resolve('babel-preset-react-app/webpack-overrides'),
-
-              plugins: [
-                [
-                  require.resolve('babel-plugin-named-asset-import'),
-                  {
-                    loaderMap: {
-                      svg: {
-                        ReactComponent: '@svgr/webpack?-prettier,-svgo![path]',
-                      },
-                    },
-                  },
-                ],
-              ],
               cacheDirectory: true,
               // Save disk space when time isn't as important
               cacheCompression: true,
               compact: true,
+              sourceMaps: false
             },
-          },
-          {
-            test: /\.(ts|tsx)$/,
-            include: paths.appSrc,
-            use: [
-              {
-                loader: require.resolve('ts-loader'),
-                options: {
-                  transpileOnly: true,
-                  experimentalWatchApi: true,
-                },
-              },
-            ],
           },
           // Process any JS outside of the app with Babel.
           // Unlike the application JS, we only compile the standard ES features.
